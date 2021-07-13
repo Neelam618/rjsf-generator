e8e852f62ea590933dd697387b4c038f60fe3b12
Neelam Chavan <neelamchavan410@gmail.com>
2021-07-13 16:58:43 +0530

    displayed schema keys seperately and submit btn hidden

--- src/components/FormViewer.js
+++ src/components/FormViewer.js
@@ -11,13 +11,29 @@ import 'antd/dist/antd.css';
 const Form = withTheme(FluentUITheme);
 
 function FormViewer(props) {
-    console.log(JSON.stringify(props.schema))
-    console.log(JSON.stringify(props.uiSchema))
-    return (
-        <div style={{ width: '80%', padding: 30}}>
-            <Form schema={props.schema} uiSchema={props.uiSchema} />
-        </div>
-    )
-}
+    // console.log(JSON.stringify(props.schema))
+    // console.log(JSON.stringify(props.uiSchema))
+    // return (
+    //     <div style={{ width: '80%', padding: 30}}>
+    //         <Form schema={props.schema} uiSchema={props.uiSchema} />
+    //     </div>
+    // )
 
+    return <div style={{ width: '80%', padding: 30}}>
+        {Object.keys(props.schema.properties).map(function(keyName) {
+            let newSchema = {"properties": {}}
+            newSchema.properties[keyName] = props.schema.properties[keyName];
+
+            let newUiSchema = {};
+            if(props.uiSchema[keyName]) {
+                newUiSchema[keyName] = props.uiSchema[keyName]
+            }
+            console.log(JSON.stringify(newSchema))
+            return (
+                <Form key={keyName} schema={newSchema} uiSchema={newUiSchema} children={true}></Form>
+            )
+        })}
+    </div>
+}
+    
 export default FormViewer
