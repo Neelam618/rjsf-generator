9bd8402de9d45abc680e1296efba3096f73dfe93
Neelam Chavan <neelamchavan410@gmail.com>
2021-08-06 12:20:52 +0530

    added styles for toolbox and formviewer

--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -4,44 +4,44 @@ function Toolbox(props) {
     return (
         <div style={{width: '30%'}}>
             <div>
-                <ul style={{paddingLeft: 0, border: "1px solid gray"}}>
-                    <li className="tools" onClick={() => props.addTxtInput()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                <ul style={{paddingLeft: 0 }}>
+                    <li className="tools" onClick={() => props.addTxtInput()} >
                         <span>Text input</span>
                     </li>
-                    <li className="tools" onClick={() => props.addNumInput()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addNumInput()} >
                         <span>Number input</span>
                     </li>
-                    <li className="tools" onClick={() => props.addSelect()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addSelect()} >
                         <span>Select</span>
                     </li>
-                    <li className="tools" onClick={() => props.addTxtarea()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addTxtarea()} >
                         <span>Text area</span>
                     </li>
-                    <li className="tools" onClick={() => props.addCheckbox()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addCheckbox()} >
                         <span>Checkbox</span>
                     </li>
-                    <li className="tools" onClick={() => props.addRadioButtons()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addRadioButtons()} >
                         <span>Radio Buttons</span>
                     </li>
-                    <li className="tools" onClick={() => props.addIntRange()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addIntRange()} >
                         <span>Integer Range</span>
                     </li>
-                    <li className="tools" onClick={() => props.addMultipleChoiceList()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addMultipleChoiceList()} >
                         <span>Multiple choice list</span>
                     </li>
-                    <li className="tools" onClick={() => props.addDate()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addDate()} >
                         <span>Date Field</span>
                     </li>
-                    <li className="tools" onClick={() => props.addDateTime()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.addDateTime()} >
                         <span>Date Time Field</span>
                     </li>
-                    {/* <li className="tools" onClick={() => props.addAltDate()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    {/* <li className="tools" onClick={() => props.addAltDate()} >
                         <span>Alternative Date Field</span>
                     </li> */}
-                    <li className="tools" onClick={() => props.chooseFile()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.chooseFile()} >
                         <span>Choose File</span>
                     </li>
-                    <li className="tools" onClick={() => props.chooseMultipleFiles()} style={{padding: 10, borderBottom: "1px solid black", listStyleType: 'none'}}>
+                    <li className="tools" onClick={() => props.chooseMultipleFiles()} >
                         <span>Choose Multiple Files</span>
                     </li>
                     </ul>
