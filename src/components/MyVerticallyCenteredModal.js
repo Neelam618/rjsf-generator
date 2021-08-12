6f999aeccfb6f8c58494077b264cb8d3c652d09d
Neelam Chavan <neelamchavan410@gmail.com>
2021-08-12 17:01:21 +0530

    added tabs in modal

--- src/components/MyVerticallyCenteredModal.js
+++ src/components/MyVerticallyCenteredModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button';
+import {Modal, Button, Tab, Tabs} from 'react-bootstrap'
 
 function MyVerticallyCenteredModal(props) {
     const schemaObj = {
@@ -11,27 +10,29 @@ function MyVerticallyCenteredModal(props) {
     };
     return (
         <>
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Schema
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <pre>
-                    {JSON.stringify(schemaObj, null, 2)}<br />
-                    {JSON.stringify(uiSchemaObj, null, 2)}
-                </pre>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+
+                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                    <Tab eventKey="home" title="schema">
+                        <pre>
+                            {JSON.stringify(schemaObj, null, 2)}
+                        </pre>
+                    </Tab>
+                    <Tab eventKey="profile" title="uiSchema">
+                        <pre>
+                            {JSON.stringify(uiSchemaObj, null, 2)}
+                        </pre>
+                    </Tab>
+                </Tabs>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
