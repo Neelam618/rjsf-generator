cab2afe55234b7709469b9ca45d48343b61161ad
Neelam Chavan <neelamchavan410@gmail.com>
2021-09-09 19:04:57 +0530

    imported navbar component library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import FormBuilder from "./components/FormBuilder";
+import Navbar from 'navbar-innerkore'
+import 'navbar-innerkore/dist/index.css'
 
 function App() {
 
   return (
     <div >
+      <Navbar />
       <FormBuilder />
     </div>
   );
